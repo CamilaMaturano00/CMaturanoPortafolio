c2a95000323b1807fb1855cf01bf857d4070daef
CamilaMaturano00 <maturano4219@gmail.com>
2022-04-26 14:59:18 -0300

    Arreglo formulario contacto

---
+++ js/form-contacto.js
@@ -0,0 +1,12 @@
+const $form = document.querySelector('#form');
+const botonMailto = document.querySelector('#direccion-enviar')
+var botonEnviar = document.querySelector(".btn-enviar");
+$form.addEventListener("submit",handleSubmit)
+
+function handleSubmit (event){
+    event.preventDefault()
+    const form = new FormData(this)
+    console.log(form.get('name'));
+    botonMailto.setAttribute('href',`mailto:maturano4219@gmail.com?subject=${form.get('name')} ${form.get('email')}&body=${form.get('message')}`);
+    botonMailto.click();
+};
